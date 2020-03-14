5eeeeb066ca93235f2977f0ba86b2cdf7713d961
gush <itcstudio@gmail.com>
2020-03-14 16:03:34 +0200

    Add and config Jest, Prettier, Webpack

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+const Warning = React.lazy(() => import('./Warning'))
+
+class App extends React.Component {
+  state = {
+    count: 0
+  }
+  render() {
+    const { count } = this.state
+    return (
+      <div>
+        <h1>Hello world!</h1>
+        <h2 className={count > 5 ? 'warning' : null}>Count: {count}</h2>
+        <button
+          onClick={() => this.setState(state => ({ count: state.count + 1 }))}
+        >
+          +
+        </button>
+        <button
+          onClick={() => this.setState(state => ({ count: state.count - 1 }))}
+        >
+          -
+        </button>
+        <br />
+        {count > 5 ? (
+          <React.Suspense fallback={null}>
+            <Warning />
+          </React.Suspense>
+        ) : null}
+      </div>
+    )
+  }
+}
+
+export default hot(module)(App)
---
+++ src/Warning.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Warning = () => <span className="warning">Take it busy!</span>
+
+export default Warning
