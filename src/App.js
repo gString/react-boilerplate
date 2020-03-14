665eba95aeac17510c7b4e37fc9fbdb581043983
gush <itcstudio@gmail.com>
2020-03-14 17:15:57 +0200

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 
-const Warning = React.lazy(() => import('./Warning'))
+// lazyload/suspence example:
+// const Warning = React.lazy(() => import('./Warning'))
 
 class App extends React.Component {
-  state = {
-    count: 0
-  }
   render() {
-    const { count } = this.state
     return (
       <div role="main">
-        <h1>Hello world!</h1>
-        <h2 className={count > 5 ? 'warning' : null}>Count: {count}</h2>
-        <button
-          onClick={() => this.setState(state => ({ count: state.count + 1 }))}
-        >
-          +
-        </button>
-        <button
-          onClick={() => this.setState(state => ({ count: state.count - 1 }))}
-        >
-          -
-        </button>
-        <br />
+        <h1>Hello World</h1>
+        {/*        
+lazyload/suspence example: 
         {count > 5 ? (
           <React.Suspense fallback={null}>
             <Warning />
           </React.Suspense>
         ) : null}
+ */}
       </div>
     )
   }
--- src/Warning.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const Warning = () => <span className="warning">Take it busy!</span>
-
-export default Warning
